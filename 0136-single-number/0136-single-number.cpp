ae697cd1622e947270753534ae94eab328b17376
Shweta <93861042+Shwet310@users.noreply.github.com>
2023-12-25 19:45:14 +0530

    Time: 20 ms (29.20%), Space: 17.1 MB (81.75%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
        int ans=0;
 	   for(auto x:nums)
-	   ans^=x;
+	   ans^=x;                // XORing a number with 0 results in the number itself 
+                             // XORing a number with itself results in 0
 	   return ans;
     }
 };
