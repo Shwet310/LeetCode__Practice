45198504c67788496af5cead82c487812eda73f3
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-03-10 22:14:58 +0530

    Time: 55 ms (26.59%), Space: 16.8 MB (53.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,14 +1,6 @@
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
-        vector<int> v;
-        for(const auto &num: set1){
-                if(set2.find(num) != set2.end()) 
-                    v.push_back(num);
-            }
-        return v;
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1.intersection(set2))
         
-    }
-};
