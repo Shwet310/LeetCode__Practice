1f85042aea50f908b708d54e4066c867fe1f1ee3
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-04-07 20:01:11 +0530

    Time: 3 ms (45.50%), Space: 7.3 MB (75.32%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -3,9 +3,18 @@ public:
     bool checkValidString(string s) {
         int leftMin =0, leftMax =0;
         for(char c : s){
-            if(c == '(') leftMin++, leftMax++;
-            else if(c == ')') leftMin--, leftMax--;
-            else leftMin--, leftMax++;
+            if(c == '('){ 
+                leftMin++;
+                leftMax++;
+                }
+            else if(c == ')'){ 
+                leftMin--;
+                leftMax--;
+                }
+            else{ 
+                leftMin--; 
+                leftMax++;
+            }
             if(leftMax < 0) return false;
             if(leftMin < 0) leftMin=0;
         }
