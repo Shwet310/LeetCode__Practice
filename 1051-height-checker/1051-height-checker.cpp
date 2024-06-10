7f20a7b2f095e2ff227aa11f642d5d813dc56b5a
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-06-10 09:56:17 +0530

    Time: 5 ms (31.17%), Space: 10.1 MB (78.64%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected(heights.begin(), heights.end());
+        sort(heights.begin(), heights.end());
+        int count = 0;
+        for(int i=0; i<heights.size(); i++){
+            if(heights[i] != expected[i])
+                count++;
+        }
+        return count;
+    }
+    
+};
