259ea470b5bab578b438e64e2ef132f3551a1850
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-06-12 23:57:03 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (5.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-      for(int i=0; i<nums.size(); i++){
-          for(int j=i; j<nums.size(); j++){
-              if(nums[i] != 0 && nums[j]== 0){
-                  swap(nums[i],nums[j]);
-                  i++;
-              }
-              if(nums[j]<nums[i]){
-                  swap(nums[i],nums[j]);
-              }
-          }
-      }
+      // for(int i=0; i<nums.size(); i++){
+      //     for(int j=i; j<nums.size(); j++){
+      //         if(nums[i] != 0 && nums[j]== 0){
+      //             swap(nums[i],nums[j]);
+      //             i++;
+      //         }
+      //         if(nums[j]<nums[i]){
+      //             swap(nums[i],nums[j]);
+      //         }
+      //     }
+      // }
+        
+    // Using Dutch National Flag Algo
+        
+        int low=0, mid=0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
