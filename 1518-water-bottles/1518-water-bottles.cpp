fefa5d84132cbf3cf691cab91b9b4e698a3fd318
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-07-07 09:23:35 +0530

    Time: 2 ms (53.84%), Space: 7 MB (72.30%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        while(numBottles >= numExchange){
+            int remainingBottles = numBottles%numExchange;
+            numBottles /= numExchange;
+            res += numBottles;
+            numBottles += remainingBottles;
+        }
+        return res;
+    }
+};
