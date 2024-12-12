8164cbc69d164d854761d6e7dcbb426552643372
Shweta <93861042+Shwet310@users.noreply.github.com>
2024-12-12 10:31:40 +0530

    Time: 3 ms (54.53%), Space: 12.9 MB (89.48%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& g, int k) {
+        make_heap(begin(g), end(g));
+        while(k--) {
+            pop_heap(begin(g), end(g));
+            g.back() = sqrt(g.back());
+            push_heap(begin(g), end(g));
+        }
+        return accumulate(begin(g), end(g), 0LL);
+    }
+};
